b261432c6fdbcb99a0d42e8350e5bebea5049776
fagurativejam <fagurativejam@proton.me>
2025-11-26 12:05:30 -0600

    fix flake syntax and allow unfree globally

--- flake.nix
+++ flake.nix
@@ -12,7 +12,9 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
-        config.allowUnfree = true;
+        config = {
+          allowUnfree = true;
+        };
     in {
       nixosConfigurations.starkiller = nixpkgs.lib.nixosSystem {
         inherit system;
