81fa98512b3b766c91f7ff97110e9629d4f9cc6b
fagurativejam <fagurativejam@proton.me>
2025-11-26 18:50:45 -0600

    testes testes 12345

--- modules/hyprland-home.nix
+++ modules/hyprland-home.nix
@@ -1,48 +1,43 @@
 { config, pkgs, lib, ... }:
 
 {
-	options.my.desktop.hyprland.enable = lib.mkEnableOption "Enable Hyprland for user";
-	
-	config = lib.mkIf config.my.desktop.hyprland.enable {
-	
-		home.packages = with pkgs; [
-			waybar
-			rofi
-		];
-		wayland.windowManager.hyprland = {
-			enable = true;
-			settings = {
-				monitor=",preferred,auto,auto";
-				exec-once = [
-					"waybar"
-				];
-				bind = [
-					"SUPER, D, exec rofi -show drun"
-				];
-			};
-		};
-		
-		programs.waybar = {
-			enable = true;
-			settings = {
-				layer = "top";
-				position = "bottom";
-				modules-left = [ "hyprland/workspaces" ];
-				modules-center = [ "clock" ];
-				modules-right = [ "pulseaudio" "network"];
-			};
-			style = ''
-				*{
-				font-family: "JetBrains Mono", monospace;
-				font-size: 12px;
-				}
-				#clock { padding: 0 10px; }
-				'';
+  options.my.desktop.hyprland.enable =
+    lib.mkEnableOption "Enable Hyprland for user";
 
-		};
-		programs.rofi = {
-			enable = true;
-			theme = "gruvbox-dark";
-		};
-	};
+  config = lib.mkIf config.my.desktop.hyprland.enable {
+    # Hyprland
+    wayland.windowManager.hyprland = {
+      enable = true;
+      settings = {
+        monitor = [ ",preferred,auto,auto" ];
+        exec-once = [ "waybar" ];
+        bind = [ "SUPER, D, exec rofi -show drun" ];
+      };
+    };
+
+    # Waybar
+    programs.waybar = {
+      enable = true;
+      settings = {
+        layer = "top";
+        position = "bottom";
+        modules-left = [ "hyprland/workspaces" ];
+        modules-center = [ "clock" ];
+        modules-right = [ "pulseaudio" "network" ];
+      };
+      style = ''
+        * {
+          font-family: "JetBrains Mono", monospace;
+          font-size: 12px;
+        }
+        #clock { padding: 0 10px; }
+      '';
+    };
+
+    # Rofi
+    programs.rofi = {
+      enable = true;
+      theme = "gruvbox-dark";
+    };
+  };
 }
