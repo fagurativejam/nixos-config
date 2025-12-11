58a53fee7f352da463a3d4da47a1aa02bb76c3de
fagurativejam <fagurativejam@proton.me>
2025-12-10 23:05:44 -0600

    reverting changes

--- flake.nix
+++ flake.nix
@@ -1,19 +1,16 @@
 {
-  description = "NixOS + Home Manager setup for starkiller and figs";
+  description = "My NixOS setup with Home Manager";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:Nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
-    let
-      system = "x86_64-linux";
-    in {
-      # --- Main host configuration ---
+  outputs = { self, nixpkgs, home-manager, ...}:
+    {
       nixosConfigurations.starkiller = nixpkgs.lib.nixosSystem {
-        inherit system;
+        system = "x86_64-linux";
         modules = [
           ./hosts/starkiller/starkiller.nix
           ./hosts/starkiller/starkiller-hardware.nix
@@ -25,40 +22,13 @@
           }
         ];
       };
-
-      # --- Installer ISO configuration ---
-      nixosConfigurations.install-iso = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          # Reuse your system config (but NOT hardware)
-          ./hosts/starkiller/starkiller.nix
-
-          # ✅ Properly import the installer ISO module from nixpkgs input
-          (import "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
-
-          home-manager.nixosModules.home-manager
-          {
-            # Avoid broken ZFS by explicitly listing supported filesystems
-            boot.supportedFilesystems = [ "btrfs" "xfs" "ext4" ];
-
-            environment.systemPackages = with nixpkgs.legacyPackages.${system}; [
-              git vim parted
-            ];
-          }
-        ];
-      };
-
-      # --- Expose ISO as a top-level package ---
-      packages.${system}.install-iso =
-        self.nixosConfigurations.install-iso.config.system.build.isoImage;
-
-      # --- Home Manager standalone config ---
       homeConfigurations.figs = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs {
-          inherit system;
+        pkgs = import nixpkgs { 
+          system = "x86_64-linux"; 
           config.allowUnfree = true;
         };
         modules = [ ./users/figs/figs.nix ];
       };
     };
+
 }
