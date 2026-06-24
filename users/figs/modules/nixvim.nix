320c6b8be44b545533f0484bd04ad29e273bf33d
fagurativejam <fagurativejam@proton.me>
2026-06-24 03:29:28 -0500

    system update

--- users/figs/modules/nixvim.nix
+++ users/figs/modules/nixvim.nix
@@ -287,6 +287,10 @@
       #   };
       # };
     };
+
+    globals.mapleader = " ";
+    globals.maplocalleader = " ";
+
     keymaps = [
       { mode = "n"; key = "<leader>ff"; action = "<cmd>Telescope find_files<cr>"; }
       { mode = "n"; key = "<leader>fg"; action = "<cmd>Telescope live_grep<cr>";}
